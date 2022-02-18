de26c58e3836504b2abcfd2d38d74d4deb6a9abe
jsmartinezv <jsmartinezv@unal.edu.co>
2022-02-18 16:23:25 -0500

    se agrego un archivo para practicar condicionales

---
+++ python/ejemplos_basicos/condicionales.py
@@ -0,0 +1,8 @@
+# Manejo de IF
+edad = int(input("Ingresa tu edad: "))
+if edad>17:
+    print("Eres mayor de edad")
+    pass # palabra reservada para indicar que voy a poner codigo despues
+else:
+    print("Eres menor de edad")
+##----------------    
