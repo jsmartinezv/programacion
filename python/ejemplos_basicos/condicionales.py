b684cb92408c97daf05176e59168d03b82feb560
jsmartinezv <jsmartinezv@unal.edu.co>
2022-02-18 16:38:20 -0500

    se modifico condicionales.py para trabajar con condicional if, else y elif

--- python/ejemplos_basicos/condicionales.py
+++ python/ejemplos_basicos/condicionales.py
@@ -1,8 +1,16 @@
 # Manejo de IF
-edad = int(input("Ingresa tu edad: "))
-if edad>17:
-    print("Eres mayor de edad")
-    pass # palabra reservada para indicar que voy a poner codigo despues
-else:
-    print("Eres menor de edad")
+#edad = int(input("Ingresa tu edad: "))
+#if edad>17:
+#    print("Eres mayor de edad")
+#    pass # palabra reservada para indicar que voy a poner codigo despues
+#else:
+#    print("Eres menor de edad")
 ##----------------    
+# Manejo de elif
+numero = int(input("Ingrese un numero: "))
+if numero > 5:
+    print("Es mayor que 5")
+elif numero == 5:
+    print("Es igual a 5")
+else:
+    print("Es menor a 5")
